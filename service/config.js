5ac5c8b2a38da9e00fa78ba86837322e6ced1c2c
SongYu <urisong@imegahome.com>
2025-06-17 08:35:44 +0800

    feat: 22

--- service/config.js
+++ service/config.js
@@ -2,5 +2,5 @@ export default {
 	// apiBaseUrl: 'https://app-server.netearn.club/v1/',
 	// apiBaseUrl: 'https://app-api.68fa.vip/v1/',
 	apiBaseUrl: 'https://app-api.jcrn.com/v1/',
-	ossBaseUrl: 'https://worldex.oss-cn-hongkong.aliyuncs.com/'
+	ossBaseUrl: 'https://jcrn.oss-us-west-1.aliyuncs.com/'
 }
